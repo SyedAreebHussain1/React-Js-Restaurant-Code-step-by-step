4bdb406fd641eb89d4118d7f40a813ba9cdf3d3c
SyedAreebHussain1 <sareeb65@gmail.com>
2022-09-17 00:01:10 +0500

    Update value with Api

---
+++ React js project in Hindi #6 Update Restaurant Entry/restaurant app/src/component/RestaurantList/RestaurantList.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import "./list.css";
+import { Table } from "react-bootstrap";
+import {  Link } from "react-router-dom";
+
+
+class RestaurantList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: null,
+    };
+  }
+  componentDidMount() {
+    // fetch promise return karta ha
+    fetch("http://localhost:3000/restaurant").then((response) => {
+      response.json().then((result) => {
+        console.log("result=>", result);
+        this.setState({ list: result });
+      });
+    });
+  }
+  render() {
+    // console.log("list=>", this.state);
+    let { list } = this.state;
+    return (
+      <div>
+        <h1>Restaurant List</h1>
+        {this.state.list ? (
+          <div>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Rating</th>
+                  <th>Location</th>
+                  <th>Operation</th>
+                </tr>
+              </thead>
+              <tbody>
+                {list.map((v, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{v.id}</td>
+                      <td>{v.name}</td>
+                      <td>{v.rating}</td>
+                      <td>{v.address}</td>
+                      <td><Link to={"/update/"+v.id}>Edit</Link></td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        ) : (
+          <p>Please wait...</p>
+        )}
+      </div>
+    );
+  }
+}
+export default RestaurantList;
